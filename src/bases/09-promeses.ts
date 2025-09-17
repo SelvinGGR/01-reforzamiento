12bd5b111fa805e12c7a10ea8785eefcad6dca55
Selvin Gonzalez <kgoenyi@gmail.com>
2025-09-17 14:28:39 -0600

    Update 09-promeses.ts

--- src/bases/09-promeses.ts
+++ src/bases/09-promeses.ts
@@ -1,28 +1,21 @@
-
-
-const myPromise = new Promise((resolve) => {
-
-
-    setTimeout (() => {
-
-    //!despues de dos segundos quiero mi dinero
-    resolve(100)
-}, 2000); //dos segundos
-
-
+const myPromise = new Promise<number>((resolve, reject) => {
+    setTimeout(() => {
+        //! después de dos segundos quiero mi dinero
+        resolve(100);
+        reject('mi migo se perdio');
+    }, 2000); // dos segundos
 });
 
-myPromise.then(
-
-    (myMoneyIsBack) => {
-
-    console.log('Tengo mi dinero' ${myMoneyIsBack})  
-
-    }
-)
-
-
-
+myPromise
+    .then((myMoneyIsBack) => {
+        console.log(`Tengo mi dinero ${myMoneyIsBack}`);
+    })
+    .catch((reason) => {
+        console.warn(reason);
+    })
+    .finally(() => {
+        console.log('pues a seguir con la vida');
+    });
 
 
 
