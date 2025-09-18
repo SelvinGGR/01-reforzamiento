4876634a3472743e287c26b2e1e8063eb593a744
Selvin Gonzalez <kgoenyi@gmail.com>
2025-09-18 14:54:06 -0600

    Update 11-async-await.ts

--- src/bases/11-async-await.ts
+++ src/bases/11-async-await.ts
@@ -2,36 +2,19 @@ import type { GiphyRandomResponse } from "../data/giphy.response";
 
 const API_KEY = 'ZSc1U0QIX7WtroWBjcs9GxWh8GsjvRHc';
 
-const myRequest = fetch(`https://api.giphy.com/v1/stickers/random?api_key=${API_KEY}`);
-
-
-
-
-const createImageInsideDOM = (url:string) =>{
-
- const imgElement = document.createElement('img');
-        imgElement.src = url;
-        document.body.append(imgElement);
+const createImageInsideDOM = (url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+    document.body.append(imgElement);
 };
 
+const getRandomImageURL = async (): Promise<string> => {
+    const response = await fetch(`https://api.giphy.com/v1/stickers/random?api_key=${API_KEY}`);
+    const json: GiphyRandomResponse = await response.json();
+    return json.data.images.original.url;
+};
 
-
-myRequest
-    .then((response) => response.json())
-    .then(({data}: GiphyRandomResponse) => {
-    
-        const imageUrl= data.images.original.url;
-     createImageInsideDOM(imageUrl);
-
-       
-
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-
-
-
+getRandomImageURL().then(url => createImageInsideDOM(url));
 
 
 
